433518d8978baf67d3d0165b2fd346ffacaa486f
Михаил Габров <mishaborovik@yandex.ru>
2023-07-01 20:35:27 +0300

    changes in command v 2

--- backend/recipes/management/commands/csvimport.py
+++ backend/recipes/management/commands/csvimport.py
@@ -7,7 +7,7 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        with open('data/ingredients.json', encoding='utf-8') as f:
+        with open('../data/ingredients.json', encoding='utf-8') as f:
             jsondata = json.load(f)
             if 'measurement_unit' in jsondata[0]:
                 for line in jsondata:
