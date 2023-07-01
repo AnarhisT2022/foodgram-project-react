f7d0085de7c1d1a16b196541f7a0f3a4bf4be880
Михаил Габров <mishaborovik@yandex.ru>
2023-07-01 03:58:19 +0300

    fix v2

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import UniqueConstraint
-from .validators import validate_username
 from rest_framework.exceptions import ValidationError
 
+from .validators import validate_username
 
 USER = 'user'
 ADMIN = 'admin'
