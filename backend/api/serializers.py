38f98c287d418374bbfb9514d8842a3191df28a8
Михаил Габров <mishaborovik@yandex.ru>
2023-07-02 16:52:08 +0300

    test

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -135,13 +135,13 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_is_favorited(self, obj):
         request = self.context.get('request')
-        return False if request else Favorite.objects.filter(
+        return not request.user.is_anonymous and Favorite.objects.filter(
             user=request.user, recipe_id=obj
         ).exists()
 
     def get_is_in_shopping_cart(self, obj):
         request = self.context.get('request')
-        return False if request else ShoppingCart.objects.filter(
+        return not request.user.is_anonymous and  ShoppingCart.objects.filter(
             user=request.user, recipe_id=obj
         ).exists()
 
